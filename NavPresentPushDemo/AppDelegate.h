aa2a69879d96dc97090f55e2a7d1c69090ee2a24
周茂 <zhoumao@meituan.com>
2015-11-15 00:52:42 +0800

    first commit

---
+++ NavPresentPushDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NavPresentPushDemo
+//
+//  Created by zhoumao on 15/11/15.
+//  Copyright © 2015年 zhoumao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) ViewController *viewController;
+
+
+@end
+
---
+++ NavPresentPushDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NavPresentPushDemo
+//
+//  Created by zhoumao on 15/11/15.
+//  Copyright © 2015年 zhoumao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
